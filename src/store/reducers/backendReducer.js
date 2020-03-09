779ffa7d75b6558111a38156d2d192c5d85e8f5b
shengitcode <shenghuiyu@yahoo.com>
2020-03-09 17:59:34 -0400

    heroku upload

--- src/store/reducers/backendReducer.js
+++ src/store/reducers/backendReducer.js
@@ -5,7 +5,7 @@ const initState = {};
 
 const backendReducer = (state = initState, action) => {
     if (action.type === 'CREATE_SOCKET') {
-        const socket = io('https://delit-back-end.herokuapp.com/https://delit-back-end.herokuapp.com/');
+        const socket = io('https://real-delit.herokuapp.com/');
         // const socket = io('127.0.0.1:5000');
 
 
