a345674e89d7bce04977b8cd93f37543659a0bd1
shengitcode <shenghuiyu@yahoo.com>
2020-03-10 01:27:23 -0400

    backend address

--- src/store/reducers/backendReducer.js
+++ src/store/reducers/backendReducer.js
@@ -5,7 +5,7 @@ const initState = {};
 
 const backendReducer = (state = initState, action) => {
     if (action.type === 'CREATE_SOCKET') {
-        const socket = io('https://real-delit.web.app/server');
+        const socket = io('https://real-delit.herokuapp.com/');
         // const socket = io('127.0.0.1:5000');
 
 
