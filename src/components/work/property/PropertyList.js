06002f962d09d495923758751bd3a4c6c638ff76
shengitcode <shenghuiyu@yahoo.com>
2020-03-15 23:26:57 -0400

    property window css

--- src/components/work/property/PropertyList.js
+++ src/components/work/property/PropertyList.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Table } from 'react-bootstrap'
 import ContentEditable from 'react-contenteditable'
 import { Button } from 'react-materialize'
+import { connect } from 'react-redux';
 
 class PropertyList extends React.Component {
 
 
     state = {
         properties: [
-            { name: 'name1', value: 'value1', nref: React.createRef(), vref: React.createRef() },
-            { name: 'name2', value: 'value2', nref: React.createRef(), vref: React.createRef() },
-            { name: 'name3', value: 'value3', nref: React.createRef(), vref: React.createRef() },
+            { name: 'name1', value: 'value1', nref: React.createRef(), vref: React.createRef(), selected: false },
+            { name: 'name2', value: 'value2', nref: React.createRef(), vref: React.createRef(), selected: false },
+            { name: 'name3', value: 'value3', nref: React.createRef(), vref: React.createRef(), selected: false },
         ]
     };
 
@@ -21,9 +22,33 @@ class PropertyList extends React.Component {
         this.setState({ properties: properties });
     };
 
+    handleSelect = (index, e) => {
+        let { properties } = this.state;
+        properties.forEach(p => p.selected = false)
+        properties[index].selected = true;
+        this.setState({ properties: properties });
+
+    }
+
+    getClassName = (index) => {
+        const { properties } = this.state;
+        if (properties[index].selected)
+            return 'table-row table-row-selected'
+        else
+            return 'table-row'
+    }
+
 
     render() {
         const { properties } = this.state;
+        let { width } = this.props;
+        if (typeof width === 'number') width += 'px'
+        width = width.split('px')[0] / 2.3
+        const style = {
+            maxWidth: width,
+            width: width,
+            flexShrink: 0,
+        }
         return (
             <>
                 <Table striped bordered hover size="sm" className="property-table">
@@ -38,23 +63,25 @@ class PropertyList extends React.Component {
                         {
                             properties && properties.map((property, index) => {
                                 return (
-                                    <tr key={index}>
-                                        <td>
+                                    <tr key={index} onClick={() => this.handleSelect(index)} className={this.getClassName(index)}>
+                                        <td style={style}>
                                             <ContentEditable
                                                 innerRef={property.nref}
                                                 onChange={this.handleChange.bind(this, index, 'name')}
                                                 html={property.name}
                                                 disabled={false}
                                                 className="property-input"
+                                                style={style}
                                             />
                                         </td>
-                                        <td>
+                                        <td style={style}>
                                             <ContentEditable
                                                 innerRef={property.vref}
                                                 onChange={this.handleChange.bind(this, index, 'value')}
                                                 html={property.value}
                                                 disabled={false}
                                                 className="property-input"
+                                                style={style}
                                             />
                                         </td>
                                     </tr>
@@ -70,4 +97,11 @@ class PropertyList extends React.Component {
     }
 }
 
-export default PropertyList;
+const mapStateToProps = (state) => {
+    const { width } = state.workScreen.property.size
+    return {
+        width: width,
+    }
+};
+
+export default connect(mapStateToProps)(PropertyList)
--- src/store/reducers/workScreenReducer.js
+++ src/store/reducers/workScreenReducer.js
@@ -6,7 +6,11 @@ import * as actionCreators from '../actions/actionCreators'
 const workScreenReducer = (state = initState, action) => {
     if (action.type === actionCreators.WINDOW_RESIZE) {
         let window = state[action.name];
-        window.size = action.value;
+        const { width, height } = action.value
+        window.size = {
+            width: parseInt(width.split('px')[0]) < 120 ? 120 + 'px' : width,
+            height,
+        }
         return {
             ...state,
             [action.name]: window,
@@ -20,23 +24,23 @@ export default workScreenReducer;
 const rect = document.body.getBoundingClientRect();
 const { width, height } = rect
 const tilesetWindow = {
-    position: { x: width * 0.8, y: height * 0.28 },
-    size: { width: width * 0.2, height: height * 0.42 },
+    position: { x: width * 0.8, y: height * 0.28 < 177.15 ? 177.15 : height * 0.28 },
+    size: { width: width * 0.2, height: height * 0.42 < 265.717 ? 265.717 : height * 0.42 },
 }
 
 const propertyWindow = {
     position: { x: 0, y: 0 },
-    size: { width: width * 0.2, height: height * 0.7 },
+    size: { width: width * 0.2, height: height * 0.7 < 442.867 ? 442.867 : height * 0.7 },
 }
 
 const mapWindow = {
     position: { x: width * 0.2, y: 0 },
-    size: { width: width * 0.6, height: height * 0.7 },
+    size: { width: width * 0.6, height: height * 0.7 < 442.867 ? 442.867 : height * 0.7 },
 }
 
 const layerWindow = {
     position: { x: width * 0.8, y: 0 },
-    size: { width: width * 0.2, height: height * 0.28 },
+    size: { width: width * 0.2, height: height * 0.28 < 177.15 ? 177.15 : height * 0.28 },
 }
 
 const initState = {
