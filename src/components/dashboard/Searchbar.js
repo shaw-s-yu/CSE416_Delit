d3118269fa2a43c18053187d95153675413c6341
RayZhaoSBU <yiwei.ray.zhao@gmail.com>
2020-03-17 14:25:56 -0400

    dashboard
    search nav

--- src/components/dashboard/Searchbar.js
+++ src/components/dashboard/Searchbar.js
@@ -5,8 +5,8 @@ import { TextInput, Button } from 'react-materialize'
 class Searchbar extends React.Component {
 
     render() {
-        const left = this.props.open ? 350 : 100;
-        const width = this.props.open ? 55 : 75;
+        const left = this.props.open ? 360 : 75;
+        const width = this.props.open ? 64 : 83;
         return (
             <div>
                 <div className="dashboard-search" style={
@@ -16,7 +16,7 @@ class Searchbar extends React.Component {
                     }}>
                     <TextInput label="Enter search" />
                 </div>
-                <Button type="submit" waves='orange' className="dashboard-searchbutton blue">Search</Button>
+                <Button type="submit" waves='orange' className="dashboard-searchbutton">Search</Button>
             </div >
 
         )
