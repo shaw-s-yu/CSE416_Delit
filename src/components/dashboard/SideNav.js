28a9ba7bac8dd4d700f50c8917fa02df0225ef2f
RayZhaoSBU <yiwei.ray.zhao@gmail.com>
2020-03-17 14:11:47 -0400

    dashboard css
    side nav

--- src/components/dashboard/SideNav.js
+++ src/components/dashboard/SideNav.js
@@ -10,22 +10,25 @@ class SideNav extends React.Component {
 
       <div className='dashboard-sidebar' style={{ width: width + "px", whiteSpace: "nowrap" }}>
         <Collection>
-          <Button
-            className="right side-clear-btn"
-            floating
-            icon={<Icon>clear</Icon>}
-            small
-            node="button"
-            waves="light"
-            onClick={this.props.handleSidebarOpen}
-          />
-          <Button className="side-create-btn" node="button" waves="light">Create New Project</Button>
+
+          {/* <Button className="side-create-btn" node="button" waves="light">Create New Project</Button> */}
+
+          <CollectionItem><div className='item-text'>Create New Project</div></CollectionItem>
 
           <CollectionItem><div className='item-text'>All Projects</div></CollectionItem>
           <CollectionItem><div className='item-text'>Project Created By Me</div></CollectionItem>
           <CollectionItem><div className='item-text'>Project Shared With Me</div></CollectionItem>
           <CollectionItem><div className='item-text'>Manage My Tilesets</div></CollectionItem>
         </Collection>
+        {/* <Button
+            className="side-clear-btn"
+            floating
+            icon={<Icon>clear</Icon>}
+            small
+            node="button"
+            waves="light"
+            onClick={this.props.handleSidebarOpen}
+          /> */}
       </div>
 
     )
