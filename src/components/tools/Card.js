3093f74666bf3b3d20e560a917e63fbd4d74d05a
shengitcode <shenghuiyu@yahoo.com>
2020-04-16 17:21:40 -0400

    dashboard better fab css hover

--- src/components/tools/Card.js
+++ src/components/tools/Card.js
@@ -7,18 +7,21 @@ class Card extends React.Component {
 
         const { className, style, name, modifiedBy, img, handleTeam, onClick } = this.props
         return (
-            <div className={className} style={style} onClick={onClick}>
-                <img src={img} className='card-preview-img' ></img>
-                <div className="card-info-box">
-                    <span className="card-info-name">Name: {name}</span>
-                    <span className="card-info-last-modify">Last modified By: {modifiedBy}</span>
-                    <div className="card-info-btn-box">
-                        <div className="card-info-btn-tl card-info-btn">Rename</div>
-                        <div className="card-info-btn-tr card-info-btn">Dupliate</div>
-                        <div className="card-info-btn-bl card-info-btn" onClick={handleTeam}>Team</div>
-                        <div className="card-info-btn-br card-info-btn">Delete</div>
-                        <div className="card-info-btn-center card-info-btn">EDIT</div>
+            <div >
+                <div className={className} style={style} onClick={onClick}>
+                    <img src={img} className='card-preview-img' ></img>
+                    <div className="card-info-box">
+                        <span className="card-info-name">Name: {name}</span>
+                        <span className="card-info-last-modify">Last modified By: {modifiedBy}</span>
+
                     </div>
+                </div>
+                <div className="card-info-btn-box" style={style}>
+                    <div className="card-info-btn-tl card-info-btn">Rename</div>
+                    <div className="card-info-btn-tr card-info-btn">Dupliate</div>
+                    <div className="card-info-btn-bl card-info-btn" onClick={handleTeam}>Team</div>
+                    <div className="card-info-btn-br card-info-btn">Delete</div>
+                    <div className="card-info-btn-center card-info-btn">EDIT</div>
                 </div>
             </div>
         )
