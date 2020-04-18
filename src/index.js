0c9f05ee6de1dbbff49d9993d658483e82a62c1b
RayZhaoSBU <yiwei.ray.zhao@gmail.com>
2020-04-18 17:37:57 -0400

    added some package

--- src/index.js
+++ src/index.js
@@ -12,11 +12,16 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import rootReducer from './store/reducers/rootReducer'
+import rootReducer from './store/reducers/rootReducer';
+import ApolloClient from 'apollo-boost';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+                    <ApolloProvider client={client}>
+                        <App />
+                    </ApolloProvider>
+                </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
