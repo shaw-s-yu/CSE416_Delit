8b8017be1d9738016b8a00dd219eff96c0798a33
shengitcode <shenghuiyu@yahoo.com>
2020-04-27 17:01:27 -0400

    Update Sidebar.js

--- client/src/components/dashboard/Sidebar.js
+++ client/src/components/dashboard/Sidebar.js
@@ -3,52 +3,14 @@ import React from 'react';
 class Sidebar extends React.Component {
 
 	state = {
-		all: true,
-		create: false,
-		share: false,
-		tileset: false,
-		addProjectDialog: false,
+		selected: 'all',
 	}
 
-	handleAllProjectsSelected = () => {
-		this.setState({
-			all: true,
-			create: false,
-			share: false,
-			tileset: false,
-		})
+	handleSelect = (type) => {
+		this.setState({ selected: type })
 	}
-
-	handleProjectCreatedByMeSelected = () => {
-		this.setState({
-			all: false,
-			create: true,
-			share: false,
-			tileset: false,
-		})
-	}
-
-	handleProjectSharedWithMeSelected = () => {
-		this.setState({
-			all: false,
-			create: false,
-			share: true,
-			tileset: false,
-		})
-	}
-
-
-	handleManageMyTilesetsSelected = () => {
-		this.setState({
-			all: false,
-			create: false,
-			share: false,
-			tileset: true,
-		})
-	}
-
 	getClassName = (name) => {
-		return this.state[name] ? "dashboard-sidebar-selected" : "";
+		return this.state.selected === name ? "dashboard-sidebar-selected" : "";
 	}
 
 	render() {
@@ -59,10 +21,10 @@ class Sidebar extends React.Component {
 			<div className='dashboard-sidebar' style={{ width: width + "%", whiteSpace: "nowrap" }}>
 				<div className='sidebar-wrapper'>
 					<div className={'sidebar-item '} onClick={this.props.handleOpen.bind(this, 'project')}><div className='item-text'>Create New Project</div></div>
-					<div className={'sidebar-item ' + this.getClassName("all")} onClick={this.handleAllProjectsSelected}><div className='item-text'>All Projects</div></div>
-					<div className={'sidebar-item ' + this.getClassName("create")} onClick={this.handleProjectCreatedByMeSelected}><div className='item-text'>Project Created By Me</div></div>
-					<div className={'sidebar-item ' + this.getClassName("share")} onClick={this.handleProjectSharedWithMeSelected}><div className='item-text'>Project Shared With Me</div></div>
-					<div className={'sidebar-item ' + this.getClassName("tileset")} onClick={this.handleManageMyTilesetsSelected}><div className='item-text'>Manage My Tilesets</div></div>
+					<div className={'sidebar-item ' + this.getClassName("all")} onClick={this.handleSelect.bind(this, 'all')}><div className='item-text'>All Projects</div></div>
+					<div className={'sidebar-item ' + this.getClassName("create")} onClick={this.handleSelect.bind(this, 'create')}><div className='item-text'>Project Created By Me</div></div>
+					<div className={'sidebar-item ' + this.getClassName("share")} onClick={this.handleSelect.bind(this, 'share')}><div className='item-text'>Project Shared With Me</div></div>
+					<div className={'sidebar-item ' + this.getClassName("tileset")} onClick={this.handleSelect.bind(this, 'tileset')}><div className='item-text'>Manage My Tilesets</div></div>
 				</div>
 			</div>
 
