08aed0a6255c8449401f4d778a745b732bf5dbee
shengitcode <shenghuiyu@yahoo.com>
2020-04-29 18:17:59 -0400

    dashboard graphql rework

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -8,6 +8,10 @@ import { connect } from 'react-redux';
 import Sidebar from "./Sidebar";
 import Dialogs from './Dialogs'
 import ProjectDialog from "./ProjectDialog";
+import QueryList from '../../graphql/Query'
+import { Query } from 'react-apollo'
+import axios from 'axios'
+import * as handler from '../../store/database/HomeScreenHandler';
 
 class Dashboard extends React.Component {
     state = {
@@ -36,14 +40,55 @@ class Dashboard extends React.Component {
         this.setState({ showSidebar: showSidebar });
     };
 
+    getQuery = () => {
+        const { selected } = this.state
+        if (selected === 'all')
+            return QueryList.GET_MY_RELATED_PROJECTS
+        if (selected === 'create')
+            return QueryList.GET_MY_OWNED_PROJECTS
+        if (selected === 'share')
+            return QueryList.GET_MY_SHARED_PROJECTS
 
+        return QueryList.EMPTY_QUERY
+    }
+
+    getProjects = (data) => {
+        const { selected } = this.state
+        if (selected === 'all')
+            return data.user.projectsRelated
+        if (selected === 'create')
+            return data.user.projectsOwned
+        if (selected === 'share')
+            return data.user.projectsShared
+        return null
+    }
+
+
+    componentDidMount() {
+        axios.get('/auth/current').then(res => {
+            if (!res.data)
+                this.props.history.push('/');
+            else {
+                this.setState({ user: res.data });
+                this.props.handleLoginSuccess(res.data);
+            }
+        })
+    }
 
     render() {
-        const { showSidebar, selected } = this.state;
+        const { showSidebar, selected, user } = this.state;
         const { history } = this.props;
         const left = showSidebar ? 19 : 0;
         const width = showSidebar ? 81 : 100;
+        if (!user) return 'loading'
+        const query = this.getQuery();
+        const displayStyle = {
+            marginLeft: left + "%",
+            width: width + "%",
+        }
+
         return (
+
             <div>
                 <TopNavbar handleSidebarOpen={this.handleSidebarOpen} site='dashboard' history={history} />
                 <Sidebar
@@ -53,28 +98,43 @@ class Dashboard extends React.Component {
                     handleSelect={this.handleSelectSide}
                     selected={selected}
                 />
-                <div className="dashboard-display" style={
-                    {
-                        marginLeft: left + "%",
-                        width: width + "%",
-                    }
-                }>
-                    <Searchbar />
-                    <ItemList
-                        history={this.props.history}
-                        handleOpen={this.handleDialogsOpen}
-                        handleClose={this.handleDialogsClose}
-                        selected={selected}
-                    />
-                    <Pagination className="dashboard-pagination center" size="large" color="secondary" />
-                </div>
-                <ProjectDialog project={this.state.project} handleClose={this.handleDialogsClose}/>
+                <Query query={query} variables={{ userId: user._id }}>
+                    {({ loading, error, data }) => {
+                        if (loading) return 'loading'
+                        if (error) return 'error'
+                        if (query === QueryList.EMPTY_QUERY)
+                            return (
+                                <div className="dashboard-display" style={displayStyle}>
+                                    'Wrong Sidebar Selection or needs to be developped
+                                </div>
+                            )
+                        if (!data) return 'error'
+
+                        const projects = this.getProjects(data)
+                        return (
+                            <div className="dashboard-display" style={displayStyle}>
+                                <Searchbar />
+                                <ItemList
+                                    history={this.props.history}
+                                    handleOpen={this.handleDialogsOpen}
+                                    handleClose={this.handleDialogsClose}
+                                    selected={selected}
+                                    projects={projects}
+                                />
+                                <Pagination className="dashboard-pagination center" size="large" color="secondary" />
+
+                            </div>
+                        )
+                    }}
+                </Query>
+                <ProjectDialog project={this.state.project} handleClose={this.handleDialogsClose} />
                 <Dialogs
                     {...this.state}
                     handleOpen={this.handleDialogsOpen}
                     handleClose={this.handleDialogsClose}
                 />
             </div >
+
         )
     }
 
@@ -82,10 +142,13 @@ class Dashboard extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
     const { history } = ownProps;
-    return { history };
+    const { user } = state.auth
+    return { history, user };
 };
 
 const mapDispatchToProps = (dispatch) => ({
+    handleLoginSuccess: (user) => dispatch(handler.loginSuccessHandler(user)),
+    handleLoginError: (errmsg) => dispatch(handler.loginErrorHandler(errmsg)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
--- client/src/components/dashboard/ItemList.js
+++ client/src/components/dashboard/ItemList.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Card from '../tools/Card'
-import QueryList from '../../graphql/Query'
-import { Query } from 'react-apollo'
+
 
 class ItemList extends React.Component {
 
@@ -16,87 +15,51 @@ class ItemList extends React.Component {
         this.props.history.push('/project/fwef')
     };
 
-    getQuery = () => {
-        const { selected } = this.props
-        if (selected === 'all')
-            return QueryList.GET_MY_RELATED_PROJECTS
-        if (selected === 'create')
-            return QueryList.GET_MY_OWNED_PROJECTS
-        if (selected === 'share')
-            return QueryList.GET_MY_SHARED_PROJECTS
 
-        return QueryList.EMPTY_QUERY
-    }
-
-    getProjects = (data) => {
-        const { selected } = this.props
-        if (selected === 'all')
-            return data.user.projectsRelated
-        if (selected === 'create')
-            return data.user.projectsOwned
-        if (selected === 'share')
-            return data.user.projectsShared
-        return null
-    }
 
     render() {
-        const { user } = this.props;
-        if (user === null) return 'loading'
-        const query = this.getQuery();
+        const { projects } = this.props;
+        const numItem = projects.length
+        const style = {
+            height: numItem > 3 ? 600 : 300
+        }
 
         return (
-            <Query query={query} variables={{ userId: user._id }}>
-                {({ loading, error, data }) => {
-                    if (loading) return 'loading'
-                    if (error) return 'error'
-                    if (query === QueryList.EMPTY_QUERY) return 'Wrong Sidebar Selection or needs to be developped'
-                    if (!data) return 'error'
 
-                    const projects = this.getProjects(data)
-                    const numItem = projects.length
-                    const style = {
-                        height: numItem > 3 ? 600 : 300
+            <div className="dashboard-itemlist">
+                <div className="dashboard-itemlist-wrapper" style={style}>
+                    {
+                        projects && projects.map((project, index) => {
+                            const col = index % 3;
+                            const row = Math.floor(index / 3);
+                            const left1s = 'calc(25% - 135px)';
+                            const left2s = 'calc(50% - 90px)';
+                            const left3s = 'calc(75% - 45px)';
+                            const top1s1 = 50;
+                            const top1s2 = 200 / 3;
+                            const top2s2 = top1s2 * 2 + 200;
+                            const cardStyle = {
+                                top: numItem > 3 ? row === 0 ? top1s2 : top2s2 : top1s1,
+                                left: col === 0 ? left1s : col === 1 ? left2s : left3s,
+                            }
+                            const { name, ownerInfo, img } = project;
+
+                            return (
+                                <Card
+                                    className='item-card'
+                                    owner={ownerInfo.username}
+                                    name={name}
+                                    style={cardStyle}
+                                    img={img}
+                                    handleOpen={this.props.handleOpen}
+                                    onClick={this.handleGoEdit}
+                                    key={index}
+                                />
+                            );
+                        })
                     }
-
-                    return (
-                        <div className="dashboard-itemlist">
-                            <div className="dashboard-itemlist-wrapper" style={style}>
-                                {
-                                    projects && projects.map((project, index) => {
-                                        const col = index % 3;
-                                        const row = Math.floor(index / 3);
-                                        const left1s = 'calc(25% - 135px)';
-                                        const left2s = 'calc(50% - 90px)';
-                                        const left3s = 'calc(75% - 45px)';
-                                        const top1s1 = 50;
-                                        const top1s2 = 200 / 3;
-                                        const top2s2 = top1s2 * 2 + 200;
-                                        const cardStyle = {
-                                            top: numItem > 3 ? row === 0 ? top1s2 : top2s2 : top1s1,
-                                            left: col === 0 ? left1s : col === 1 ? left2s : left3s,
-                                        }
-                                        const { name, ownerInfo, img } = project;
-
-                                        return (
-                                            <Card
-                                                className='item-card'
-                                                owner={ownerInfo.username}
-                                                name={name}
-                                                style={cardStyle}
-                                                img={img}
-                                                handleOpen={this.props.handleOpen}
-                                                onClick={this.handleGoEdit}
-                                                key={index}
-                                            />
-                                        );
-                                    })
-                                }
-                            </div>
-                        </div>
-                    )
-                }}
-            </Query>
-
+                </div>
+            </div>
         )
     }
 
