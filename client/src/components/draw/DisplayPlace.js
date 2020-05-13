afee48cc36aa794c0a17de0f09d2883463c7065d
shengitcode <shenghuiyu@yahoo.com>
2020-05-12 23:12:31 -0400

    Update DisplayPlace.js

--- client/src/components/draw/DisplayPlace.js
+++ client/src/components/draw/DisplayPlace.js
@@ -426,15 +426,16 @@ class DisplayPlace extends React.Component {
             this.GridController.drawGrid()
             this.painter.setDimension(canvasWidth, canvasHeight)
             const { imageId } = this.props.tileset
-            if (imageId !== '' && imageId !== '5eacb076d0ed064dec138c41')
+            if (imageId !== '')
                 axios.get(`/data/image?imageId=${imageId}`).then(res => {
                     const { err, msg, data } = res.data
                     if (err)
                         console.log(msg)
                     else {
                         const base64Flag = 'data:image/jpeg;base64,';
-                        if (!data) return
-                        const imageStr = arrayBufferToBase64(data.data.data)
+                        console.log(data)
+                        if (!data.data) return
+                        const imageStr = arrayBufferToBase64(data.data)
                         this.handleDrawImgToGrid(base64Flag + imageStr)
                     }
                 })
